a80fe41969cdf3b5bbfe88b672fd6a2b6a513efc
Ellie Priestley <ellie.priestley@gmail.com>
2023-12-14 16:06:10 +0000

    Changed print statements in playGame for a more interesting user experience

--- src/main/kotlin/unscramble/UnscrambleGame.kt
+++ src/main/kotlin/unscramble/UnscrambleGame.kt
@@ -19,13 +19,22 @@ class UnscrambleGame {
     fun playGame() {
         val word = wordRandomiser(wordList)
         val scrambledWord = scrambleWord(word)
-        println("Here is a random scrambled word: $scrambledWord")
-        println("Can you unscramble it? What is your guess? : ")
+        println("\n" +
+                "  _   _                                    _     _      \n" +
+                " | | | |_ __  ___  ___ _ __ __ _ _ __ ___ | |__ | | ___ \n" +
+                " | | | | '_ \\/ __|/ __| '__/ _` | '_ ` _ \\| '_ \\| |/ _ \\\n" +
+                " | |_| | | | \\__ \\ (__| | | (_| | | | | | | |_) | |  __/\n" +
+                "  \\___/|_| |_|___/\\___|_|  \\__,_|_| |_| |_|_.__/|_|\\___|\n" +
+                "                                                        \n")
+        println("________________________________________________________")
+        println("\nThis is the Unscramble word game.👋\n\nYour goal is to unscramble a 5-letter word.\n\nYou have one guess...\n\nReady?\n\nHere is your random scrambled word: $scrambledWord")
+        println("\nCan you unscramble it? Input your guess below 👇 and press Enter: ")
+
         val userGuess = readln()
         if (isGuessCorrect(userGuess, word)) {
-            println("Correct! Well done, you've beaten the game")
+            println("\nCorrect! Well done, you've beaten the game! 🥳")
         } else {
-            println("Oh no! The correct answer was $word. You lost...")
+            println("\nOh no! The correct answer was '$word'. Better luck next time! 😬")
         }
     }
 }
